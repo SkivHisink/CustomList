1b431db252aea5795a00f92e878bad174e055854
Skiv27rus <skiv27rus@gmail.com>
2021-07-26 11:19:15 +0700

    Deleted unneccessary includes

--- CustomList/CList.cs
+++ CustomList/CList.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CustomList
 {
